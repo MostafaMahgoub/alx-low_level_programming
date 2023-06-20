0ec1be6dfbb35a111a19e45b38d09022fef6708e
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 13:11:27 -0700

    Test fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf('+1');
+		printf("+%d\n", 1);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf('0');
+		printf("%d\n", 0);
 		return (0);
 	}
 	else
 	{
-		printf('-1');
+		printf("%d\n", -1);
 		return (-1);
 	}
 }
