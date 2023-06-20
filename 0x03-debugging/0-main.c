bc5eeb2afa0659f8b5679ad9fe0c9db87dfd4f98
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 14:15:21 -0700

    Test main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - tests the positive_or_negative function with an input value of 0
- *
- * Return: Always 0.
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-    int i = 0;
+	int i;
 
-    positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
