40ee7c6cdc2d3ad6d6a81844b5f7929418a52096
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 13:37:10 -0700

    Test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,24 +5,21 @@
  *
  * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
  */
-int print_sign(int n)
+void print_sign(int n)
 {
     if (n > 0)
     {
         putchar('+');
 	putchar('1');
-        return (1);
     }
     else if (n == 0)
     {
         putchar('0');
 	putchar('0');
-        return (0);
     }
     else
     {
         putchar('-');
 	putchar('1');
-        return (-1);
     }
 }
