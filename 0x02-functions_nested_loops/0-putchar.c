b3875578379be034da4e1e99307fa9b41b6a8cb7
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 12:49:01 -0700

    Quick fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
+/*
+ * Desc: put putchar with newline
+ */
 int main(void)
 {
-    char c[] = "_putchar\n";
-    int i;
+	char c[] = "_putchar\n";
+	int i;
 
-    for (i = 0; c[i] != '\0'; i++)
-    {
-        putchar(c[i]);
-    }
-
-    return 0;
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		putchar(c[i]);
+	}
+	return 0;
 }
