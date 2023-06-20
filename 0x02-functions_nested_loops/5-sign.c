bd0d718ba19363f380aaf3ff1ff9cbc4f4f95427
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 13:08:38 -0700

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		putchar('+1');
 		return (1);
 	}
 	else if (n == 0)
@@ -18,7 +18,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		putchar('-');
+		putchar('-1');
 		return (-1);
 	}
 }
