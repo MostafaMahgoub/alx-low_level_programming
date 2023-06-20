7ef0b73dc732c0680108f4be02e5059e94c1f8f8
MostafaReda <mostafamahgoub@outlook.com>
2023-06-20 13:33:42 -0700

    Test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
     if (n > 0)
     {
-        putchar('+');
+        putchar('+1');
         return (1);
     }
     else if (n == 0)
     {
-        putchar('0');
+        putchar('00');
         return (0);
     }
     else
     {
-        putchar('-');
+        putchar('-1');
         return (-1);
     }
 }
